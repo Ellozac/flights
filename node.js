8317db2d541983989d588b56f865148d9ec80047
Ellozac <zlsy108@gmail.com>
2023-07-15 22:44:24 +1200

    You can now sign in

--- node.js
+++ node.js
@@ -1,12 +1,34 @@
 const sqlite = require('sqlite3');
 const express = require('express');
+const bcrypt = require('bcrypt');
 const app = express();
-
+const passHash = "$2b$10$cI8NmAN0RpgA/RSwu55QX.n4XFOBquCGiEh1fFFu8HnXZVkODynjq"
 let db = new sqlite.Database('./database.sqlite');
 db.run("CREATE TABLE IF NOT EXISTS flights (id INTEGER PRIMARYKEY AUTO INCREMENT);")
 
 
 app.use(express.static('./public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// POST route for "/devLog"
+app.post('/devLog', (req, res) => {
+   // Retrieve the username and password from the request body
+   const { username, password } = req.body;
+   
+   // Perform authentication or any other necessary logic
+   // Here, for demonstration purposes, we're simply checking if the username and password match a predefined value
+   bcrypt.compare(password, passHash, (err, outcome) => {
+      if (outcome && username == "QJZ") {
+         res.send("LOG");
+
+      } else {
+         res.status(401).send('Invalid username or password');
+       }
+   });
+
+ });
+
 
 
 
@@ -16,3 +38,6 @@ var server = app.listen(8081, function () {
 
    console.log("Website is here :) -->http://%s:%s", host, port);
 });
+
+
+
--- public/script.js
+++ public/script.js
@@ -1,16 +1,63 @@
+let usernamePassword 
 var slideIndex = 0;
 
 function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.opacity = "0";
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.opacity = "1";
-  setTimeout(carousel, 4000); 
+  if ( window.location.pathname == "/index.html"){
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.opacity = "0";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.opacity = "1";
+    setTimeout(carousel, 4000); 
+  };
 }
 
+
+// function submitLogin() {
+//   const username = document.getElementById('usernameField').value;
+//   const password = document.getElementById('passwordField').value;
+//   usernamePassword = { username, password };
+ 
+  
+// }
+if (window.location.pathname == "/subpages/devLog.html"){
+  document.getElementById("loginForm").addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Get form data
+    const username = document.getElementById('usernameField').value;
+    const password = document.getElementById('passwordField').value;
+    const usernamePassword = { username, password };
+    console.log(usernamePassword);
+
+    // Perform your custom logic here
+
+    // Make the fetch request to "/devLog" when the form is submitted
+    fetch("/devLog", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(usernamePassword)
+    })
+      .then(response => response.text())
+      .then(data => {
+        // Handle the response data
+        console.log(data); // Log the response from the server
+        if (data === 'LOG') {
+          // Redirect to a success page or perform other actions
+          window.location.href = '../restrictedSubs/addFlight.html';
+        } else {
+          // Display an error message or perform other actions
+          console.log('Login failed');
+          alert("INCORRECT DETAILS")
+        }
+      });
+  });
+}
 carousel();
+
 
