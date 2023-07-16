5ad2beebfc3c429b506e1539fba31f24312a6505
Ellozac <zlsy108@gmail.com>
2023-07-16 14:28:16 +1200

    NEOW SIENG IN

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const sqlite = require('sqlite3');
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const app = express();
-const passHash = "$2b$10$cI8NmAN0RpgA/RSwu55QX.n4XFOBquCGiEh1fFFu8HnXZVkODynjq"
+const passHash = "$2b$10$cI8NmAN0RpgA/RSwu55QX.n4XFOBquCGiEh1fFFu8HnXZVkODynjq";
+const secret_key = "12EUFJNR0s2RfuNxz3TbnTP5yqbJusHCkJ5WoQt1Ms849NvJ5nGWDEBg4zjF";
 let db = new sqlite.Database('./database.sqlite');
-db.run("CREATE TABLE IF NOT EXISTS flights (id INTEGER PRIMARYKEY AUTO INCREMENT);")
-
+db.run("CREATE TABLE IF NOT EXISTS flights (id INTEGER PRIMARYKEY AUTO INCREMENT);");
 
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: false }));
@@ -15,29 +16,50 @@ app.use(express.json());
 app.post('/devLog', (req, res) => {
    // Retrieve the username and password from the request body
    const { username, password } = req.body;
-   
+
    // Perform authentication or any other necessary logic
    // Here, for demonstration purposes, we're simply checking if the username and password match a predefined value
    bcrypt.compare(password, passHash, (err, outcome) => {
       if (outcome && username == "QJZ") {
-         res.send("LOG");
+         const token = jwt.sign({ username }, secret_key, { expiresIn: '1h' });
 
+         // Send the token in the response
+         res.json({ token });
       } else {
          res.status(401).send('Invalid username or password');
-       }
+      }
    });
+});
 
+app.post('/validateToken', (req, res) => {
+   const token = req.headers.authorization?.split(' ')[1];
+ 
+   // Check if the token exists
+   if (!token) {
+     return res.status(401).json({ valid: false });
+   }
+ 
+   try {
+     // Verify the token
+     jwt.verify(token, secret_key, (err, decoded) => {
+       if (err) {
+         // Token verification failed
+         return res.status(401).json({ valid: false });
+       } else {
+         // Token is valid
+         return res.json({ valid: true });
+       }
+     });
+   } catch (error) {
+     // Error occurred during token verification
+     return res.status(500).json({ error: 'Internal server error' });
+   }
  });
-
-
-
-
+ 
+ 
 var server = app.listen(8081, function () {
    var host = server.address().address;
    var port = server.address().port;
 
-   console.log("Website is here :) -->http://%s:%s", host, port);
+   console.log("Website is here :) --> http://%s:%s", host, port);
 });
-
-
-
--- public/script.js
+++ public/script.js
@@ -16,13 +16,6 @@ function carousel() {
 }
 
 
-// function submitLogin() {
-//   const username = document.getElementById('usernameField').value;
-//   const password = document.getElementById('passwordField').value;
-//   usernamePassword = { username, password };
- 
-  
-// }
 if (window.location.pathname == "/subpages/devLog.html"){
   document.getElementById("loginForm").addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the default form submission
@@ -43,21 +36,57 @@ if (window.location.pathname == "/subpages/devLog.html"){
       },
       body: JSON.stringify(usernamePassword)
     })
-      .then(response => response.text())
+      .then(response => response.json())
       .then(data => {
         // Handle the response data
         console.log(data); // Log the response from the server
-        if (data === 'LOG') {
+        if (data.token) {
+          localStorage.setItem("token", data.token);
           // Redirect to a success page or perform other actions
-          window.location.href = '../restrictedSubs/addFlight.html';
+          window.location.href = "../restrictedSubs/addFlight.html";
         } else {
           // Display an error message or perform other actions
-          console.log('Login failed');
-          alert("INCORRECT DETAILS")
+          console.log("Login failed");
+          alert("INCORRECT DETAILS");
         }
+        
       });
   });
 }
+if (window.location.pathname == '/restrictedSubs/addFlight.html') {
+  const token = localStorage.getItem('token');
+  if (token) {
+    // Make a fetch request to the server to validate the token
+    fetch("/validateToken", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.valid) {
+          // Token is valid, allow the user to access restricted content or perform other actions
+          console.log('Authenticated');
+        } else {
+          // Token is invalid or expired, redirect to the login page or perform other actions
+          console.log('Token is invalid or expired');
+          window.location.href = '/subpages/devLog.html';
+        }
+      })
+      .catch(error => {
+        // Error occurred during token validation, handle the error appropriately
+        console.log('Error:', error);
+      });
+  } else {
+    // No token found, redirect to the login page or perform other actions
+    console.log('Not authenticated');
+    window.location.href = '/subpages/devLog.html';
+  }
+}
+
+
 carousel();
 
 
