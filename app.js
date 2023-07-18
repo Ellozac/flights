892872b2cbecd19165d8542d4afefa7c31fc029f
ellozac <zlsy108@gmail.com>
2023-07-18 15:04:14 +1200

    commit before school end

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const sqlite = require('sqlite3')
 const app = express();
 require("dotenv").config();
 const passHash = process.env.PASSHASH;
